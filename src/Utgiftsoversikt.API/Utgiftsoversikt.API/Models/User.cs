cb9983309893b9103a5f0ee581c430e2c44f1b29
SiriKSlyk <siri.slyk@bouvet.no>
2024-09-13 09:59:00 +0200

    add .net project

--- src/Utgiftsoversikt.API/Utgiftsoversikt.API/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace Utgiftsoversikt
-{
-    public class User
-    {
-        public int Id {get; set; }
-        public string Name {get; set; }
-        public decimal Age {get; set; }
-    }
-}
