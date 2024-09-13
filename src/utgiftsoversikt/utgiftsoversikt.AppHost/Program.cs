100a6677854c419488418b9db678d4a9573b86dd
SiriKSlyk <siri.slyk@bouvet.no>
2024-09-13 10:26:12 +0200

    add sql database

--- src/utgiftsoversikt/utgiftsoversikt.AppHost/Program.cs
+++ src/utgiftsoversikt/utgiftsoversikt.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var db = builder.AddSqlServer("sql").AddDatabase("db");
 
-
-builder.AddProject<Projects.utgiftsoversikt>("utgiftsoversikt");
+builder.AddProject<Projects.utgiftsoversikt>("utgiftsoversikt").WithReference(db);
 
 builder.Build().Run();
